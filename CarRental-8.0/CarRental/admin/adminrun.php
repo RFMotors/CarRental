8f23b5c1ed92cde0d5c88ed488b1b1adfeef3023
Doz1E <147884627+Doz1E@users.noreply.github.com>
2025-04-02 12:01:41 +0100

    Delete CarRental-8.0 directory

--- CarRental-8.0/CarRental/admin/adminrun.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = 'admin123';
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-echo "Your hashed password: " . $hashedPassword;
-?>
--- CarRental-8.0/CarRental/layout/footer.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="/css/headerFooter.css">
-<footer>
-    <p>&copy; 2025 RF Motors Rental Service. All rights reserved.</p>
-    <p>Contact us: <a href="mailto:rfmotors@tudublin.ie">rfmotors@tudublin.ie</a></p>
-</footer>
